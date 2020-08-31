a7bb2a2ce87759c729490cab68b9e0a934aca1fc
Spencer Kenealy <kenealys@me.com>
2020-08-31 11:24:49 -0700

    Post functionality created

--- routes/api/post.js
+++ routes/api/post.js
@@ -4,47 +4,43 @@ const { check, validationResult } = require("express-validator");
 const authMid = require("../../middleware/authMid");
 const db = require("../../config/db.config");
 
-
 // post
 // api/post
 // create post
 // private
-// @ts-ignore
 router.post(
   "/",
+  authMid,
   [
-    authMid,
-    [
-      check("post_text", "A post must have between 1 & 280 characters")
-        .trim()
-        .isLength({ min: 1, max: 280 }),
-    ]
+    check("post_text", "Please include text when making a post")
+      .not()
+      .isEmpty(),
+    check(
+      "post_text",
+      "A post must not be longer than 280 characters"
+    ).isLength({ max: 280 }),
   ],
-  async (req, res) => {
+  (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    let user_id = req.user.id;
+    let user_id = req.user.user_id;
     let post_text = req.body.post_text;
+    let newPostData = { user_id, post_text };
 
-    try {
-      let sql = "INSERT INTO posts ( user_id, post_text ) ?";
+    let successMsg;
 
-      await new Promise((resolve, reject) => {
-        db.query(sql, [user_id, post_text], (err, row) => {
-          if (err) {
-            return reject(err);
-          } else {
-            resolve(row);
-            res.json({ msg: "This was a success" });
-          }
-        });
-      });
-    } catch (err) {
-      console.log(err);
-    }
+    db.query("INSERT INTO posts SET ?", newPostData, (err) => {
+      if (err) {
+        console.log(err.message);
+      } else {
+        successMsg = "Post Successfully Made";
+        res.json({ msg: successMsg });
+      }
+    });
   }
 );
 
